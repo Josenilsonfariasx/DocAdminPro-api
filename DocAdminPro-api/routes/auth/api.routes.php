10318090401907ef619783f64e165722ef122c0a
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-18 20:06:55 -0300

    feat: adding routes

--- DocAdminPro-api/routes/auth/api.routes.php
+++ DocAdminPro-api/routes/auth/api.routes.php
@@ -2,7 +2,13 @@
 // Routes
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\ConfirmCodeForgotPasswordController;
+use App\Http\Controllers\Auth\ForgotPasswordContoller;
+use App\Http\Controllers\Auth\NewPasswordController;
 use Illuminate\Support\Facades\Route;
 
 // ----------POST----------
 Route::post('/login',[AuthController::class, 'login']);
+Route::post('/password/',[ForgotPasswordContoller::class, 'handle']);
+Route::post('/password/confirm',[ConfirmCodeForgotPasswordController::class, 'confirm']);
+Route::post('/password/reset',[NewPasswordController::class, 'reset']);
