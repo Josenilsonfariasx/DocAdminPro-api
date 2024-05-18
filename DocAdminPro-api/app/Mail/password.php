5ccb37be16a1a5402b678ee24ed790c8487a6dcf
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-18 20:16:14 -0300

    feat: merge

---
+++ DocAdminPro-api/app/Mail/password.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class password extends Mailable
+{
+    use Queueable, SerializesModels;
+    private $data;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Recuperação de Senha',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'email.password',
+            with: [
+                'name' => $this->data['name'],
+                'code' => $this->data['code'],
+                'from' => $this->data['from'],
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
