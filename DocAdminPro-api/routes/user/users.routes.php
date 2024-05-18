bb1e5555e42dfdc2eb3b6cc301458701d2a51a13
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-18 10:54:33 -0300

    feat: adding routes

--- DocAdminPro-api/routes/user/users.routes.php
+++ DocAdminPro-api/routes/user/users.routes.php
@@ -2,9 +2,16 @@
 // Routes
 
 use App\Http\Controllers\User\CreateUserController;
+use App\Http\Controllers\User\DeleteUserController;
+use App\Http\Controllers\User\ListAllUsersController;
 use App\Http\Controllers\User\UpdateUserController;
 use Illuminate\Support\Facades\Route;
 
 // ----------POST----------
-Route::post('/',[CreateUserController::class,'create']);
+Route::post('/create',[CreateUserController::class,'create']);
+// ----------PUT----------
 Route::put('/{id}',[UpdateUserController::class,'update']);
+// ----------DELETE----------
+Route::delete('/{id}',[DeleteUserController::class,'delete']);
+// ----------GET-------------
+Route::get('/',[ListAllUsersController::class,'listAll']);
