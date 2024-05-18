7497e0c08c02e2005df1be2d363a072b266b6bb3
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-18 10:11:09 -0300

    feat: adding handle errors with appError

--- DocAdminPro-api/bootstrap/app.php
+++ DocAdminPro-api/bootstrap/app.php
@@ -1,18 +1,40 @@
 <?php
 
+use App\Exceptions\AppError;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
+        using: function () {
+          Route::middleware('api')
+              ->prefix('api')
+              ->group(base_path('routes/api.php'));
+          Route::middleware('web')
+              ->group(base_path('routes/web.php'));
+      },
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware) {  
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $error, Request $request) {
+          if($error instanceof ValidationException){
+            return response()->json([
+                'errors' => $error->validator->errors()
+            ], 422);
+          }
+            if($error instanceof AppError){
+                return response()->json([
+                    'errors' => $error->getMessage()
+                ], $error->getCode());
+            }
+        });
     })->create();
