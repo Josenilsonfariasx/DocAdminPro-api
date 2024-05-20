1dd6e1a2c6306c6ae66e0f8fbb5e456bcda080fa
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-20 02:55:09 -0300

    feat: adding routes

--- DocAdminPro-api/routes/Document/doc.routes.php
+++ DocAdminPro-api/routes/Document/doc.routes.php
@@ -1,6 +1,9 @@
 <?php
 
+
 use App\Http\Controllers\Document\CreateDocumentController;
+use App\Http\Controllers\Document\DeleteDocumentController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/save/{id}',[CreateDocumentController::class,'save']);
+Route::delete('/delete/{id}',[DeleteDocumentController::class,'delete']);
