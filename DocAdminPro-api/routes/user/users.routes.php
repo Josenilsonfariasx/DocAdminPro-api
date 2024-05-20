06a63d55b1f7151e55985ae37bd061dfedab1ebb
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-20 02:29:40 -0300

    feat: adding route for render pdf by file_name

--- DocAdminPro-api/routes/user/users.routes.php
+++ DocAdminPro-api/routes/user/users.routes.php
@@ -10,11 +10,14 @@ use App\Http\Controllers\User\NumberAllUserSystemController;
 use App\Http\Controllers\User\UpdateUserController;
 use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 // ----------POST------------------
 Route::post('/create',[CreateUserController::class,'create']);
 Route::get('/',[ListAllUsersController::class,'listAll']);
-
+Route::get('/storage/pdfs/{filename}', function ($filename) {
+  return Storage::response("pdfs/$filename");
+});
 
 // ----------Middleware------------------------------------------------------------
 Route::middleware([JwtMiddleware::class])->group(function(){
@@ -22,11 +25,11 @@ Route::middleware([JwtMiddleware::class])->group(function(){
   Route::put('/{id}',[UpdateUserController::class,'update']);
   // ----------DELETE--------------
   Route::delete('/{id}',[DeleteUserController::class,'delete']);
-
-// ----------Filters---------------
+  
+  // ----------Filters---------------
   Route::group([
-      'prefix' => 'filter',
-    ], function(){
+    'prefix' => 'filter',
+  ], function(){
       Route::get('/docs/{id}', [GetAllDocsByUserController::class,'handle']);
       Route::get('/employees/count',[NumberAllUserSystemController::class,'numberUser']); 
       Route::get('/{id}',[GetUserByIdController::class,'get']);
