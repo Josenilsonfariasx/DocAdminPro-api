9c54f0a452813b2ac284049622e851be893a87fc
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-20 02:29:21 -0300

    feat: list all docs

--- DocAdminPro-api/app/Services/User/GetAllDocsByUserService.php
+++ DocAdminPro-api/app/Services/User/GetAllDocsByUserService.php
@@ -4,6 +4,7 @@ namespace App\Services\User;
 use App\Exceptions\AppError;
 use App\Models\Document;
 use App\Models\User;
+use Illuminate\Support\Facades\Storage;
 
 class GetAllDocsByUserService {
   public function execute (String $user_id){
@@ -11,11 +12,15 @@ class GetAllDocsByUserService {
     if(!$user) throw new AppError('user not found.', 404);
     if(!$user_id) throw new AppError('Missing user_id.', 400);
 
-    $documents = Document::where('user_id', $user_id)->select(['id','file_name','file_path','size','type','ocr_status','created_at','updated_at'])->get();
+    $documents = Document::where('user_id', $user_id)
+      ->select(['id','file_name','file_path','size','type','ocr_status','created_at','updated_at'])
+      ->get();
+
     $response = [
         'documents' => $documents,
         'count' => $documents->count(),
     ];
-      return $response;
+
+    return $response;
   }
 }
