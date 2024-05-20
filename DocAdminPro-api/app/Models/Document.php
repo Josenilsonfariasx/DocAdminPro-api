d02dd7c8d46d4621fdf09014f08ad6df5e8cc182
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-19 22:19:44 -0300

    feat: adding model Document

--- DocAdminPro-api/app/Models/Document.php
+++ DocAdminPro-api/app/Models/Document.php
@@ -12,7 +12,7 @@ class Document extends Model
     use HasFactory, Notifiable, HasUuids;
 
     protected $fillable = [
-        'filename',
+        'file_name',
         'file_path',
         'size',
         'user_id',
