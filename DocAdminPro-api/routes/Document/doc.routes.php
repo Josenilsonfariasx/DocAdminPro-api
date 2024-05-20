580876ceb19d78d2231811055ec2fadbcf57b04a
Josenilsonfariasx <fariaslwork@gmail.com>
2024-05-20 16:11:35 -0300

    fix: adding jwt verify in router docs

--- DocAdminPro-api/routes/Document/doc.routes.php
+++ DocAdminPro-api/routes/Document/doc.routes.php
@@ -4,8 +4,12 @@
 use App\Http\Controllers\Document\CreateDocumentController;
 use App\Http\Controllers\Document\DeleteDocumentController;
 use App\Http\Controllers\Document\EditDocumentName;
+use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/save/{id}',[CreateDocumentController::class,'save']);
-Route::delete('/delete/{id}',[DeleteDocumentController::class,'delete']);
-Route::put('/edit/{id}', [EditDocumentName::class, 'rename']);
+
+Route::middleware([JwtMiddleware::class])->group(function(){
+  Route::post('/save/{id}',[CreateDocumentController::class,'save']);
+  Route::delete('/delete/{id}',[DeleteDocumentController::class,'delete']);
+  Route::put('/edit/{id}', [EditDocumentName::class, 'rename']);
+});
